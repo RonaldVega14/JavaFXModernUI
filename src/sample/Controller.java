343ba705c619a3c731bd3c7709ad33ed8f87a6a6
Ronald Vega <ronald.vega48@gmail.com>
2020-07-04 15:43:23 -0600

    Agregando un poco de interaccion

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -103,12 +104,26 @@ public class Controller implements Initializable {
 
     @FXML
     private void ExplainProcess(ActionEvent event){
-
     }
 
     @FXML
-    private void showInfo(ActionEvent event){
+    private void showInfo1(ActionEvent event){
+        InfoBox.display("Informacion", "Definir el dominio consiste en definir el cuerpo sobre el cual se va a realizar el calculo");
+    }
 
+    @FXML
+    private void showInfo2(ActionEvent event){
+        InfoBox.display("Informacion", "El proceso de mallado consiste en separar el cuerpo en varias partes, creando asi un conjunto finito de elementos y nodos. Para este caso cada elemento de la malla representa un tetrahedro con 4 nodos.\n");
+    }
+
+    @FXML
+    private void showInfo3(ActionEvent event){
+        InfoBox.display("Informacion", "En una interpolacion existe una variable de interes que necesitamos interpolar (En este caso A y B). Esta variable(s) de interes depende de dos conjuntos de datos. \n1. Valores puntuales de la variable de interes, \n2. Una serie de funciones, tambien conocidas como funciones de interpolacion o funciones de forma que provoque que cada uno de los puntos interpolados siga respetando la geometria del cuerpo. ");
+    }
+
+    @FXML
+    private void showInfo4(ActionEvent event){
+        InfoBox.display("Informacion", "Condicion 1: Significa que la suma de todas las funciones de forma debe de ser igual a 1.\nCondicion 2: Al principio asumimos que tenemos los valores en los nodos, por lo que las funciones de forma evaluadas en ese punto deben de respetar ese valor inicial asumido.");
     }
 
     @FXML
@@ -116,7 +131,7 @@ public class Controller implements Initializable {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("conectividades.fxml"));
             Scene scene = new Scene(root);
-            InfoBox.display("Tabla de Conectividades", scene);
+            InfoBox.displayTable("Tabla de Conectividades", scene);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/sample/InfoBox.java
+++ src/sample/InfoBox.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,17 +11,36 @@ import javafx.stage.Stage;
 
 public class InfoBox {
 
-    public static void display(String title, Scene scene){
+    public static void display(String title, String message){
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(350);
+        window.setMinHeight(250);
+
+        Label label = new Label();
+        label.setText(message);
+
+        Button close = new Button("Close");
+        close.setOnAction(e -> window.close());
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(label, close);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+    public static void displayTable(String title, Scene scene){
         Stage window = new Stage();
 
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(450);
         window.setMinHeight(650);
-
-
-        Button close = new Button("Close the window");
-        close.setOnAction(e -> window.close());
 
         window.setScene(scene);
         window.showAndWait();
